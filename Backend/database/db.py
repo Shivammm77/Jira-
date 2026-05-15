dfb417a818bc24c9aebd01a1bb6c61b90d3a19f9
Shivammm77 <learncodewithme9@gmail.com>
2026-05-15 12:57:27 +0530

    change in sql url

--- Backend/database/db.py
+++ Backend/database/db.py
@@ -5,9 +5,9 @@ from dotenv import load_dotenv
 import os 
 
 load_dotenv()
-sql_path2 = os.getenv("sql_path")
+DATABASE_URL = os.getenv("DATABASE_URL")
 
-engine = create_engine(sql_path2)
+engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autoflush=False , autocommit = False , bind= engine)
 Base = declarative_base()
 
