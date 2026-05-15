c8d367704ea2831c721e01647ef1a3f9a2e4ed40
Shivammm77 <learncodewithme9@gmail.com>
2026-05-15 16:01:03 +0530

    Deploy frontend

--- Backend/main.py
+++ Backend/main.py
@@ -18,6 +18,7 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:5173",   # React frontend
         "http://127.0.0.1:3000",
+        "https://jira-sk.vercel.app"
     ],
     allow_credentials=True,
     allow_methods=["*"],
