316eff0a885d746776f74e71672c475106021503
Shivammm77 <learncodewithme9@gmail.com>
2026-05-15 13:14:02 +0530

    changes

--- Backend/database/db.py
+++ Backend/database/db.py
@@ -5,7 +5,7 @@ from dotenv import load_dotenv
 import os 
 
 load_dotenv()
-DATABASE_URL = os.getenv("DATABASE_URL1")
+DATABASE_URL = os.getenv("DATABASE_URL")
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autoflush=False , autocommit = False , bind= engine)
